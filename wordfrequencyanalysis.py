1eceda46ae8d380af4770bb279e20be77e6c70ea
narseoh <134836127+narseoh@users.noreply.github.com>
2024-07-08 18:49:34 +0200

    Update wordfrequencyanalysis.py

--- wordfrequencyanalysis.py
+++ wordfrequencyanalysis.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-st.title("Hello World")
-st.write("This is a simple Streamlit app.")
+# Titre de l'application
+st.title('Test')
+
+# Instructions pour l'utilisateur
+st.write('Veuillez entrer des URLs, une par ligne :')
+
+# Champ de texte pour entrer les URLs
+urls_input = st.text_area('Entrée des URLs')
+
+# Affichage des URLs saisies
+if st.button('Afficher les URLs'):
+    urls = urls_input.split('\n')
+    st.write('Voici les URLs que vous avez entrées :')
+    for url in urls:
+        st.write(url)
